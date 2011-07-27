eb406fa2208945eaaa21c83bdd3277e6f6a44b56
Steven Merrill <steven.merrill@gmail.com>
2011-07-27 01:21:34 -0300

    Make a symlink; change pwd to /tmp for most of the invocation, make the script overall more idempotent.

--- manifests/drush.pp
+++ manifests/drush.pp
@@ -1,32 +1,34 @@
 class drush {
-    exec { "download_drush":
-    cwd => "/bin",
+  exec { "download_drush":
+    cwd => "/tmp",
     command => "/usr/bin/wget http://ftp.drupal.org/files/projects/drush-7.x-4.4.tar.gz",
-    creates => "/bin/drush-7.x-4.4.tar.gz",
+    creates => "/opt/drush",
     require => Package["wget"],
   }
-  
+
   exec { "unpack_drush":
-    cwd => "/bin",
-    command => "/bin/tar xvzf /bin/drush-7.x-4.4.tar.gz",
-    creates => "/bin/drush",
+    cwd => "/tmp",
+    command => "/bin/tar xvzf /tmp/drush-7.x-4.4.tar.gz",
+    creates => "/opt/drush",
     require => Exec["download_drush"],
   }
-  
+
   exec { "remove_tar":
-    cwd => "/bin",
-    command => "/bin/rm /bin/drush-7.x-4.4.tar.gz",
+    cwd => "/tmp",
+    command => "/bin/rm /tmp/drush-7.x-4.4.tar.gz",
+    creates => "/opt/drush",
     require => [ Exec["download_drush"], Exec["unpack_drush"] ],
   }
-  
+
   exec { "install_drush":
-    cwd => "/bin",
+    cwd => "/tmp",
     command => "/bin/mv drush/ /opt/",
     creates => "/opt/drush",
     require => [ Exec["download_drush"], Exec["unpack_drush"] ],
   }
-  
+
   file { "/usr/local/bin/drush":
-    ensure => "/opt/drush/drush",
+    ensure => link,
+    target => "/opt/drush/drush",
   }
 }
